4a7ed3cbe5c0065e6826728b89f80b8392786acc
Khan Thompson <khan.thompson@pointsbet.com>
2024-05-05 11:13:48 +1000

    Don't override lsp with treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -441,10 +441,10 @@ require('nvim-treesitter.configs').setup {
     swap = {
       enable = true,
       swap_next = {
-        ['<leader>a'] = '@parameter.inner',
+        ['<leader>sa'] = '@parameter.inner',
       },
       swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
+        ['<leader>sA'] = '@parameter.inner',
       },
     },
   },
