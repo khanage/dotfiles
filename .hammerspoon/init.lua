ec417dc137b90d7cde6cca6cefde405610de243b
Khan Thompson <khan.thompson@pointsbet.com>
2025-01-16 09:40:15 +1100

    Update to hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,5 @@
 launch = hs.application.launchOrFocus
-hs.hotkey.bind({ "cmd", "shift" }, "1", function() launch("iTerm") end)
+hs.hotkey.bind({ "cmd", "shift" }, "1", function() launch("Alacritty") end)
 hs.hotkey.bind({ "cmd", "shift" }, "3", function() launch("Slack") end)
 hs.hotkey.bind({ "cmd", "shift" }, "2", function() launch("Firefox") end)
 hs.hotkey.bind({ "cmd", "shift" }, "4", function() launch("Microsoft Outlook") end)
