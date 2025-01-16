1e6e52654b6f450860dd93ce6d8bddf470142f88
Khan Thompson <khan.thompson@pointsbet.com>
2025-01-16 13:43:20 +1100

    Add csharp root directory detection

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,5 +4,22 @@ return {
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
 
     keys[#keys + 1] = { "<leader>a", vim.lsp.buf.code_action, desc = "Code [A]ction" }
+
+    setup = {
+      csharp_ls = function(_server, opts)
+        require("omnisharp").setup({
+          server = opts,
+          root_dir = function(startpath)
+            local lspconfig = require("lspconfig")
+            return lspconfig.util.root_pattern("*.sln")(startpath)
+              or lspconfig.util.root_pattern("*.csproj")(startpath)
+              or lspconfig.util.root_pattern("*.fsproj")(startpath)
+              or lspconfig.util.root_pattern(".git")(startpath)
+          end,
+        })
+
+        return true
+      end,
+    }
   end,
 }
