1a1812e70337ed15d0f830b901fdf39d0be3ece4
khanage <khanage@gmail.com>
2025-12-08 21:14:11 +1100

    Add keyboard flashing utility

--- homepc/home-manager/home.nix
+++ homepc/home-manager/home.nix
@@ -51,6 +51,7 @@
       beam28Packages.elixir
       beam28Packages.elixir-ls
       beam28Packages.erlang
+      wally-cli
       # bevy_cli.packakges.${pkgs.system}.bevy_cli
       # # You can also create simple shell scripts directly inside your
       # # configuration. For example, this adds a command 'my-hello' to your
