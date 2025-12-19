ed6444827be0d12035b50793a4f83e552fb4b95a
Khan Thompson <khan.thompson@pointsbet.com>
2025-12-19 21:25:00 +1100

    Initial work laptop setup

---
+++ test/home-manager/work.nix
@@ -0,0 +1,166 @@
+{pkgs, ...}: {
+  imports = [
+  ];
+
+  fonts.fontconfig.enable = true;
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home = {
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "25.05"; # Please read the comment before changing.
+
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = with pkgs; [
+      pkg-config
+      openssl
+      gcc
+      lsd
+      fd
+      ripgrep
+      lazygit
+      xh
+      imagemagick
+      kubectl
+      nerd-fonts.iosevka-term
+      nerd-fonts.go-mono
+    ];
+
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
+
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+      # ".config/hypr/hyprland.conf".source = ./conf/hyprland.conf;
+    };
+
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/khan/etc/profile.d/hm-session-vars.sh
+    #
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+
+    shellAliases = let
+      dotfiles = "~/dotfiles";
+    in {
+      ls = "lsd -A";
+      nbs = "sudo darwin-rebuild switch --flake ${dotfiles}/test";
+      nfu = "sudo nix flake update --flake ${dotfiles}/test && git -C ${dotfiles} commit -am 'chore: update flake lock' && git -C ${dotfiles} push";
+      pushdots = "git -C ${dotfiles} commit -am 'chore: sync dotfiles' && git -C ${dotfiles} push";
+      vimdotfiles = "vim --cmd ':cd ${dotfiles}/test/' ${dotfiles}/test/home-manager/work.nix";
+      replace-commit = "${dotfiles}/.local/bin/replace-commit";
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs = {
+    home-manager.enable = true;
+    k9s.enable = true;
+    bottom.enable = true;
+
+    alacritty = {
+      enable = true;
+      theme = "Nord";
+      settings = {
+        font = {
+          normal = {
+            family = "GoMono Nerd Font Mono";
+            style = "Regular";
+          };
+          bold = {
+            family = "GoMono Nerd Font Mono";
+            style = "Bold";
+          };
+          italic = {
+            family = "GoMono Nerd Font Mono";
+            style = "Italic";
+          };
+          bold_italic = {
+            family = "GoMono Nerd Font Mono";
+            style = "Bold Italic";
+          };
+          size = 12;
+        };
+      };
+    };
+
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    zsh = {
+      enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = ["git" "direnv" "fzf" "vi-mode"];
+      };
+      shellAliases = {
+        ls = "lsd -A";
+        vim = "nvim";
+        cat = "bat";
+      };
+      plugins = [
+      ];
+    };
+
+    git = {
+      enable = true;
+      settings = {
+        user.name = "Khan Thompson";
+        user.email = "khan.thompson@pointsbet.com";
+        init.defaultBranch = "main";
+        credential.helper = "store";
+      };
+    };
+
+    fzf = {
+      enable = true;
+    };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    nvf = (import ./nvf.nix) pkgs;
+
+    bat = {
+      enable = true;
+      config = {
+        theme = "Nord";
+        pager = "";
+      };
+    };
+  };
+}
