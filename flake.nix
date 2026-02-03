ba9c2c3df158676dbe6329070fe6411278fa9921
Khan Thompson <khan.thompson@pointsbet.com>
2026-02-03 16:25:46 +1100

    chore: update applied

--- flake.nix
+++ flake.nix
@@ -186,7 +186,7 @@
         nix.enable = false;
 
         # Custom Determinate Nix settings written to /etc/nix/nix.custom.conf
-        determinate-nix.customSettings = {
+        determinateNix.customSettings = {
           # Enables parallel evaluation (remove this setting or set the value to 1 to disable)
           eval-cores = 0;
           extra-experimental-features = [
