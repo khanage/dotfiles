21d7740fa8844851e228676d1c061a9223b48783
khanage <khanage@gmail.com>
2026-02-16 23:29:21 +1100

    chore: sync dotfiles

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -86,16 +86,6 @@
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
     file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
       ".config/hypr/hyprland.conf".source = ./conf/hyprland.conf;
       ".config/hypr/hypridle.conf".source = ./conf/hypridle.conf;
       ".config/niri/config.kdl".source = ./conf/niri/config.kdl;
