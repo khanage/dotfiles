3b50220e1ef063f0c3a137a1dacd3811d3883b31
khanage <khanage@gmail.com>
2026-02-16 23:11:53 +1100

    chore: sync dotfiles

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -129,7 +129,7 @@
       dotfiles = "~/dotfiles";
     in {
       ls = "lsd -A";
-      nbs = "sudo nixos-rebuild switch --flake ${dotfiles}";
+      nbs = "sudo nixos-rebuild switch --flake ${dotfiles} && git -C ${dotfiles} commit -am 'chore: sync dotfiles' && git -C ${dotfiles} push";
       nfu = "sudo nix flake update --flake ${dotfiles}";
       pushdots = "git -C ${dotfiles} commit -am 'chore: sync dotfiles' && git -C ${dotfiles} push";
       vdf = "vim --cmd ':cd ${dotfiles}/' ${dotfiles}/home-manager/home.nix";
@@ -141,57 +141,7 @@
   programs = {
     home-manager.enable = true;
     noctalia-shell = import ./noctalia.nix {};
-    wofi = {
-      enable = true;
-      style = ''
-        * {
-        	font-family: "Hack", monospace;
-        }
-
-        window {
-        	background-color: #3B4252;
-        }
-
-        #input {
-        	margin: 5px;
-        	border-radius: 0px;
-        	border: none;
-        	background-color: #3B4252;
-        	color: white;
-        }
-
-        #inner-box {
-        	background-color: #383C4A;
-        }
-
-        #outer-box {
-        	margin: 2px;
-        	padding: 10px;
-        	background-color: #383C4A;
-        }
-
-        #scroll {
-        	margin: 5px;
-        }
-
-        #text {
-        	padding: 4px;
-        	color: white;
-        }
-
-        #entry:nth-child(even){
-        	background-color: #404552;
-        }
-
-        #entry:selected {
-        	background-color: #4C566A;
-        }
-
-        #text:selected {
-        	background: transparent;
-        }
-      '';
-    };
+    wofi = import ./wofi.nix;
     k9s.enable = true;
     bottom.enable = true;
 
