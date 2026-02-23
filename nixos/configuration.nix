b17e0bff0dc9dd1f61ea6194cfaad3b7935dc406
khanage <khanage@gmail.com>
2026-02-23 17:54:56 +1100

    chore: sync dotfiles

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -119,8 +119,6 @@
     pulse.enable = true;
   };
 
-  services.input-remapper.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.khan = {
     isNormalUser = true;
