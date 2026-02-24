f202faef5c978fa8c2562ed619ab8a01c0c68dba
khanage <khanage@gmail.com>
2026-02-24 22:29:19 +1100

    chore: sync dotfiles

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -1,10 +1,11 @@
 {pkgs, ...}: {
   enable = true;
-  enableUpdateCheck = false;
-  enableExtensionUpdateCheck = false;
   mutableExtensionsDir = false;
 
   profiles.default = {
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+
     userSettings = {
       "window.autoDetectColorScheme" = true;
       "workbench.preferredDarkColorTheme" = "Nord";
@@ -18,17 +19,17 @@
         command = "workbench.action.terminal.toggleTerminal";
       }
     ];
-  };
 
-  extensions = with pkgs.vscode-marketplace; [
-    rust-lang.rust-analyzer
-    tamasfe.even-better-toml
-    editorconfig.editorconfig
-    a5huynh.vscode-ron
-    arcticicestudio.nord-visual-studio-code
-    jnoortheen.nix-ide
-    vscodevim.vim
-    ms-dotnettools.csdevkit
-    fill-labs.dependi
-  ];
+    extensions = with pkgs.vscode-marketplace; [
+      rust-lang.rust-analyzer
+      tamasfe.even-better-toml
+      editorconfig.editorconfig
+      a5huynh.vscode-ron
+      arcticicestudio.nord-visual-studio-code
+      jnoortheen.nix-ide
+      vscodevim.vim
+      ms-dotnettools.csdevkit
+      fill-labs.dependi
+    ];
+  };
 }
