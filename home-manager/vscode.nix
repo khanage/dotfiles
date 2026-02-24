183856b7f3ad537586efe4ca1f5a85ddb21737d5
khanage <khanage@gmail.com>
2026-02-24 21:08:20 +1100

    chore: sync dotfiles

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -3,5 +3,8 @@
   extensions = with pkgs.vscode-marketplace; [
     dracula-theme.theme-dracula
     rust-lang.rust-analyzer
+    tamasfe.even-better-toml
+    editorconfig.editorconfig
+    a5huynh.vscode-ron
   ];
 }
