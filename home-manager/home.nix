6e1e22aa0a4aa5e5ef564f6aef394a5bbbbd306a
khanage <khanage@gmail.com>
2026-02-28 16:52:16 +1100

    chore: sync dotfiles

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -69,6 +69,7 @@
             sdk_10_0
           ])
         blender
+        unrar-free
         # bevy_cli.packakges.${pkgs.system}.bevy_cli
         # # You can also create simple shell scripts directly inside your
         # # configuration. For example, this adds a command 'my-hello' to your
