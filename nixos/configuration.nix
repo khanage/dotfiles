13f541c185f8fb3dd78daaea02fe5fa73d4d673c
khanage <khanage@gmail.com>
2026-03-17 20:47:58 +1100

    chore: sync dotfiles

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -209,6 +209,7 @@
     path = [pkgs.flatpak];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      flatpak upgrade
     '';
   };
 
