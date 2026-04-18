8d606bd189881f1c033180ebd6d49306785f0f2d
Khan <khan@example.com>
2026-04-18 17:05:15 +1000

    chore: sync dotfiles

--- modules/features/steam.nix
+++ modules/features/steam.nix
@@ -1,11 +1,16 @@
 _: {
   flake.nixosModules.steam = {pkgs, ...}: {
-    programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      extraPackages = with pkgs; [gamemode];
+    programs = {
+      gamemode.enable = true;
+      gamescope.enable = true;
+      steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+        protontricks.enable = true;
+        extraPackages = with pkgs; [gamemode];
+      };
     };
   };
 }
