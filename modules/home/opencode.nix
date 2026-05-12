9891ae149e5627da88bf16f9efea6faede5733da
Khan Thompson <khan.thompson@pointsbet.com>
2026-05-12 11:41:44 +1000

    chore: sync dotfiles

--- modules/home/opencode.nix
+++ modules/home/opencode.nix
@@ -5,7 +5,7 @@
     ...
   }: let
     ado-mcp = pkgs.writeShellScriptBin "ado-mcp" ''
-      export PATH=${pkgs.lib.makeBinPath [pkgs.nodejs_20 pkgs.azure-cli]}:$PATH
+      export PATH=${pkgs.lib.makeBinPath [pkgs.nodejs_24 pkgs.azure-cli]}:$PATH
       exec ${pkgs.nodejs_20}/bin/npx -y @azure-devops/mcp "pointsbet" "$@"
     '';
   in {
