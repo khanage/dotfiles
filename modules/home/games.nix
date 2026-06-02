f98673c9985c6fb4341e099b2058e4e772e64aef
Khan <khan@example.com>
2026-06-02 19:11:55 +1000

    chore: sync dotfiles

--- modules/home/games.nix
+++ modules/home/games.nix
@@ -3,15 +3,6 @@ _: {
     home.packages = with pkgs; [
       wowup-cf
       xivlauncher
-      (lutris.override {
-        extraLibraries = pkgs: with pkgs; [
-          vulkan-loader
-        ];
-        extraPkgs = pkgs: with pkgs; [
-          vulkan-tools
-          winetricks
-        ];
-      })
     ];
   };
 }
