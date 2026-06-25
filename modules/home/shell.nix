f2439b18e4fc89214fafed2e6ed2e7baaef2b486
khanage <khanage@gmail.com>
2026-06-25 11:48:49 +1000

    chore: sync dotfiles

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -31,7 +31,7 @@ _: {
           ls = "lsd -A";
           vim = "nvim";
           cat = "bat";
-          vdf = "vim --cmd ':cd ${dotfiles}/' ${dotfiles}/modules/home/shell.nix ${dotfiles}/**/$1";
+          vdf = "vim --cmd ':cd ${dotfiles}/' ${dotfiles}/modules/home/shell.nix";
           nfu = "git -C ${dotfiles} pull && sudo nix flake update --flake ${dotfiles}";
           pushdots = "git -C ${dotfiles} commit -am 'chore: sync dotfiles' && git -C ${dotfiles} push";
           replace-commit = "${dotfiles}/.local/bin/replace-commit";
