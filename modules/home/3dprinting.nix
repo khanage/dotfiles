0fe382cfd3f177ac92226894cbdb23861c4ffc11
khanage <khanage@gmail.com>
2026-07-05 13:10:52 +1000

    chore: sync dotfiles

--- modules/home/3dprinting.nix
+++ modules/home/3dprinting.nix
@@ -2,7 +2,10 @@ _: {
   flake.homeModules.three_d_printing = {pkgs, ...}: {
     home.packages = with pkgs; [
       freecad-wayland
-      bambu-studio
+      (bambu-studio.override
+        {
+          withNvidiaGLWorkaround = true;
+        })
     ];
   };
 }
