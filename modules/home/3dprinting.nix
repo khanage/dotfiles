eedd3e82ccda9f0dd458e5751abb156643ab59a6
khanage <khanage@gmail.com>
2026-07-17 19:44:28 +1000

    chore: sync dotfiles

--- modules/home/3dprinting.nix
+++ modules/home/3dprinting.nix
@@ -2,10 +2,20 @@ _: {
   flake.homeModules.three_d_printing = {pkgs, ...}: {
     home.packages = with pkgs; [
       # freecad-wayland
-      (bambu-studio.override
+      ((bambu-studio.override
         {
           withNvidiaGLWorkaround = true;
-        })
+        }).overrideAttrs (old: {
+        # withNvidiaGLWorkaround routes through Mesa+zink, but without
+        # restricting the Vulkan ICD, zink picks llvmpipe (software) instead
+        # of the NVIDIA GPU, causing OOM crashes. Force the NVIDIA Vulkan ICD.
+        postFixup =
+          (old.postFixup or "")
+          + ''
+            wrapProgram $out/bin/bambu-studio \
+              --set VK_ICD_FILENAMES /run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json
+          '';
+      }))
     ];
   };
 }
