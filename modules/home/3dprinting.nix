abc3c401676ccdf6b3de1544fd8e12e121169f31
khanage <khanage@gmail.com>
2026-07-17 22:12:34 +1000

    chore: sync dotfiles

--- modules/home/3dprinting.nix
+++ modules/home/3dprinting.nix
@@ -2,20 +2,11 @@ _: {
   flake.homeModules.three_d_printing = {pkgs, ...}: {
     home.packages = with pkgs; [
       # freecad-wayland
-      ((bambu-studio.override
-        {
-          withNvidiaGLWorkaround = true;
-        }).overrideAttrs (old: {
-        # withNvidiaGLWorkaround routes through Mesa+zink, but without
-        # restricting the Vulkan ICD, zink picks llvmpipe (software) instead
-        # of the NVIDIA GPU, causing OOM crashes. Force the NVIDIA Vulkan ICD.
-        postFixup =
-          (old.postFixup or "")
-          + ''
-            wrapProgram $out/bin/bambu-studio \
-              --set VK_ICD_FILENAMES /run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json
-          '';
-      }))
+      # withNvidiaGLWorkaround routes through Mesa+Zink which exhausts BAR
+      # (heap 3) memory during slicing, causing OOM crashes. Use native NVIDIA
+      # OpenGL instead. If the 3D viewport appears blank, re-enable this.
+      # See: https://github.com/NixOS/nixpkgs/issues/498311
+      bambu-studio
     ];
   };
 }
