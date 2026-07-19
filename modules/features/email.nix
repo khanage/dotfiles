92c8e5528d77c60a22ad8d193b55069f64e270de
khanage <khanage@gmail.com>
2026-07-19 18:28:09 +1000

    chore: sync dotfiles

--- modules/features/email.nix
+++ modules/features/email.nix
@@ -1,8 +1,22 @@
 _: {
-  flake.nixosModules.email = {pkgs, ...}: {
+  flake.nixosModules.email = {
+    pkgs,
+    lib,
+    ...
+  }: {
     environment.systemPackages = [pkgs.mailspring];
     programs.seahorse.enable = true;
     services.gnome.gnome-keyring.enable = true;
     security.pam.services.login.enableGnomeKeyring = true;
+    systemd.user.services.mailspring = {
+      description = "Mailspring";
+      after = ["graphical-session.target"];
+      partOf = ["graphical-session.target"];
+      wantedBy = ["graphical-session.target"];
+      serviceConfig = {
+        ExecStart = "${lib.getExe pkgs.mailspring} --background";
+        Restart = "on-failure";
+      };
+    };
   };
 }
