a00e6a85776d4477983081e01ffae616fde3e181
khanage <khanage@gmail.com>
2026-07-19 12:56:00 +1000

    chore: sync dotfiles

--- modules/features/email.nix
+++ modules/features/email.nix
@@ -1,5 +1,7 @@
 _: {
   flake.nixosModules.email = {pkgs, ...}: {
     environment.systemPackages = [pkgs.mailspring];
+    programs.seahorse.enable = true;
+    services.gnome.gnome-keyring.enable = true;
   };
 }
