f1e37de3732edb065274f1bfec1e7a1191d5a376
khanage <khanage@gmail.com>
2026-07-20 16:44:14 +1000

    chore: sync dotfiles

--- modules/features/sqlServer.nix
+++ modules/features/sqlServer.nix
@@ -1,10 +1,5 @@
 _: {
   flake.darwinModules.sqlServer = _: {
-    environment.etc."krb5.conf" = {
-      # mode = "0644";
-      # user = "root";
-      # group = "wheel";
-      source = /run/secrets/krb5_conf;
-    };
+    environment.etc."krb5.conf".source = /run/secrets/krb5_conf;
   };
 }
