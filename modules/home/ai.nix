ff9c27b4a2297d7e9aadefe65cce8625ff03fec2
khanage <khanage@gmail.com>
2026-07-21 13:02:38 +1000

    chore: sync dotfiles

--- modules/home/ai.nix
+++ modules/home/ai.nix
@@ -10,20 +10,6 @@ _: {
       // optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
         acceleration = "cuda";
         package = pkgs.ollama-cuda;
-      }
-      // optionalAttrs pkgs.stdenv.hostPlatform.isDarwin
-      {
-        package = pkgs.ollama.overrideAttrs (oldAttrs: {
-          # Unable to load mlx on apple hardware, and setting DYLOAD path has no effect.
-          postPatch =
-            (oldAttrs.postPatch or "")
-            + ''
-              # Find where Ollama defines its rigid Apple runner search array
-              # and forcefully append the Nix store MLX library path into the lookups
-              substituteInPlace discover/gpu_darwin.go \
-                --replace '"/usr/local/lib"', '"${pkgs.python3Packages.mlx}/lib", "/usr/local/lib"'
-            '';
-        });
       };
   };
 }
