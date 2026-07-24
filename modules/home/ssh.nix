28dd296ff19ef6e786c89e756acc3f3750451cc6
khanage <khanage@gmail.com>
2026-07-24 12:55:34 +1000

    chore: sync dotfiles

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -3,15 +3,11 @@ _: {
     programs.ssh = {
       enable = true;
 
-      matchBlocks = {
-        # Personal GitHub account (khanage). Force ONLY this identity so ssh
-        # doesn't offer the default/work keys first.
-        "github.com-personal" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = "/run/secrets/github_personal_ssh_key";
-          identitiesOnly = true;
-        };
+      settings."github.com-personal" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "/run/secrets/github_personal_ssh_key";
+        identitiesOnly = true;
       };
     };
   };
