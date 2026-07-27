fc9d6979cf1f34dbcd70a880afe1b0b32c6b24f5
khanage <khanage@gmail.com>
2026-07-27 10:27:03 +1000

    chore: sync dotfiles

--- modules/home/ai.nix
+++ modules/home/ai.nix
@@ -4,7 +4,7 @@ _: {
   in {
     services.ollama =
       {
-        enable = true;
+        enable = false;
         environmentVariables = {OLLAMA_CONTEXT_LENGTH = "64000";};
       }
       // optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
