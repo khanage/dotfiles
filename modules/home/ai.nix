73c7419ba1aa6b05e757a9192537355a70543326
khanage <khanage@gmail.com>
2026-08-09 21:53:17 +1000

    chore: sync dotfiles

--- modules/home/ai.nix
+++ modules/home/ai.nix
@@ -10,14 +10,6 @@ _: {
       // optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
         acceleration = "cuda";
         package = pkgs.ollama-cuda;
-        # package = pkgs.ollama-cuda.overrideAttrs (old: {
-        #   # The nested llama.cpp build does not inherit the parent CMake flags,
-        #   # and the CUDA setup hook gives it a malformed toolkit root.
-        #   preBuild = ''
-        #     export CUDAToolkit_ROOT="${pkgs.cudaPackages.cuda_nvcc}"
-        #     ${old.preBuild}
-        #   '';
-        # });
       };
   };
 }
