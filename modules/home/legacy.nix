37205c8f282985ac24c0c11c94cc310b664119e3
khanage <khanage@gmail.com>
2026-08-18 00:14:54 +1000

    chore: sync dotfiles

--- modules/home/legacy.nix
+++ modules/home/legacy.nix
@@ -159,5 +159,20 @@ _: {
         TimeoutStopSec = 10;
       };
     };
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = ["firefox.desktop"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/about" = ["firefox.desktop"];
+        "x-scheme-handler/unknown" = ["firefox.desktop"];
+        "x-scheme-handler/mailto" = ["Mailspring.desktop"];
+      };
+      associations.added = {
+        "x-scheme-handler/mailto" = ["Mailspring.desktop"];
+      };
+    };
   };
 }
